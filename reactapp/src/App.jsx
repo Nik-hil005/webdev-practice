6e1ec6f9d92f37a84668ad171268d22422c6af5c
Nikhil Bhiduri <nikhilbhiduri2005@gmail.com>
2026-04-06 10:51:23 +0530

    feat: props and useState

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-import {add,subtract} from './utility'
 import News from './component/Sample'
+import HomePage from './component/HomePage'
 
 
 function App() {
